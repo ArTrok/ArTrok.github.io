80490535221a07cf23a20b296d0d8e08fb824854
Arthur <arthurteixeira0805@gmail.com>
2022-02-24 14:15:13 -0300

    ✅ 🚑 FIX: funcking fixed mofos

--- src/tests/MainPage.test.js
+++ src/tests/MainPage.test.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import { screen } from '@testing-library/react';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 const NAV_BUTTONS = ['Home_b', 'About_b', 'Projects_b', 'Skills_b', 'Contact_b'];
@@ -10,10 +8,10 @@ describe('Main Page', () => {
   //   const { history } = renderWithRouter(<App />);
   // });
   it('should have nav buttons', () => {
-    renderWithRouter(<App />);
-  //   for (const texts of NAV_BUTTONS) {
-  //     const navButtons = screen.getByTestId(texts);
-  //     expect(navButtons).toBeInTheDocument();
-    // }
+    renderWithRouter('/');
+    for (const texts of NAV_BUTTONS) {
+      const navButtons = screen.getByTestId(texts);
+      expect(navButtons).toBeDefined();
+    }
   });
 });
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import App from '../App';
 
-const renderWithRouter = (component) => {
+const renderWithRouter = (path) => {
   const history = createMemoryHistory();
+  history.push(path);
   return ({
-    ...render(<Router history={ history }>{ component }</Router>), history,
+    ...render(<Router history={ history }><App /></Router>), history,
   });
 };
 
