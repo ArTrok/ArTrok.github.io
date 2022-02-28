e9e171ebec6d0fe2e80634503cb647c2e89d785e
Arthur <arthurteixeira0805@gmail.com>
2022-02-28 19:29:36 -0300

    ✨ ✅ feat: added images e fixed tests

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import MadeByMeSeal from '../components/MadeByMeSeal';
 import NavBar from '../components/NavBar';
+import FunPic from '../images/meDoingPullUps.gif';
+import OldPic from '../images/beforeAndAfter.jpg';
+import RecentPic from '../images/nowCropped.jpg';
 
 const About = () => {
   return (
     <>
     <NavBar />
     <div className='main_content_container'>
-      <p id='description'>
-        Born in <em>Sao Paulo City, Brazil,</em> Arthur is a person with a lot of determination
-        and is trying to be the change in the world doing what he can with his knowledge
-        and physical capabilities. Always updating himself with information from cutting
-        edge news hubs most of them focused in tech news. Discovered his abilities as a
-        <strong>developer</strong> after graduting in chemical engineering and not finding a job that would
-        make him feel fulfilled.
-      </p> 
+      <div>
+        <p id='description'>
+          Born in <em>Sao Paulo City, Brazil,</em> Arthur is a person with a lot of determination
+          and is trying to be the change in the world doing what he can with his knowledge
+          and physical capabilities. Always updating himself with information from cutting
+          edge news hubs most of them focused in tech news. Discovered his abilities as a
+          <strong>developer</strong> after graduting in chemical engineering and not finding a job that would
+          make him feel fulfilled.
+        </p>
+        <img src={ FunPic } alt='Me doing pull ups' />
+      </div>
+      <div>
+        <img src={ OldPic } alt='before and after of myself' />
+        <p id='description'>
+          Born in <em>Sao Paulo City, Brazil,</em> Arthur is a person with a lot of determination
+          and is trying to be the change in the world doing what he can with his knowledge
+          and physical capabilities. Always updating himself with information from cutting
+          edge news hubs most of them focused in tech news. Discovered his abilities as a
+          <strong>developer</strong> after graduting in chemical engineering and not finding a job that would
+          make him feel fulfilled.
+        </p>
+      </div>
+      <div>
+        <p id='description'>
+          Born in <em>Sao Paulo City, Brazil,</em> Arthur is a person with a lot of determination
+          and is trying to be the change in the world doing what he can with his knowledge
+          and physical capabilities. Always updating himself with information from cutting
+          edge news hubs most of them focused in tech news. Discovered his abilities as a
+          <strong>developer</strong> after graduting in chemical engineering and not finding a job that would
+          make him feel fulfilled.
+        </p>
+        <img src={ RecentPic } alt='me smiling in front of a computer ' />
+      </div>
     </div>
     <MadeByMeSeal />
     </>
--- src/tests/AboutPage.test.js
+++ src/tests/AboutPage.test.js
@@ -7,7 +7,7 @@ describe('Main Page', () => {
   // beforeEach(() => {
   // });
   it('should have nav buttons', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     for (const texts of NAV_BUTTONS) {
       const navButtons = screen.getByTestId(texts);
       expect(navButtons).toBeDefined();
@@ -15,43 +15,43 @@ describe('Main Page', () => {
   });
 
   it('should have a fun/funny picture', () => {
-    renderWithRouter('/');
-    const profilePic = screen.getByTestId('profile_pic');
-    expect(profilePic).toBeDefined();
+    renderWithRouter('/about');
+    const firstPic = screen.getByTestId('first_pic');
+    expect(firstPic).toBeDefined();
   });
 
   it('should have an old picture', () => {
-    renderWithRouter('/');
-    const profilePic = screen.getByTestId('profile_pic');
-    expect(profilePic).toBeDefined();
+    renderWithRouter('/about');
+    const secondPic = screen.getByTestId('second_pic');
+    expect(secondPic).toBeDefined();
   });
 
   it('should have a recent picture', () => {
-    renderWithRouter('/');
-    const profilePic = screen.getByTestId('profile_pic');
-    expect(profilePic).toBeDefined();
+    renderWithRouter('/about');
+    const thirdPic = screen.getByTestId('third_pic');
+    expect(thirdPic).toBeDefined();
   });
 
   it('should have general context description', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     const briefDescription = screen.getByTestId('brief_description1');
     expect(briefDescription).toBeDefined();
   });
 
   it('should have text description about my past', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     const briefDescription = screen.getByTestId('brief_description2');
     expect(briefDescription).toBeDefined();
   });
 
   it('should have text description about my goals and future', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     const briefDescription = screen.getByTestId('brief_description3');
     expect(briefDescription).toBeDefined();
   });
 
   it('should have "watermark" at the end of the page', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     const madeByMeSeal = screen.getByTestId('made_by');
     const madeByMeSealImg = screen.getByRole('img', { name: 'madeByMeSeal' });
     expect(madeByMeSeal).toBeDefined();
@@ -59,7 +59,7 @@ describe('Main Page', () => {
   });
 
   it('should have an emote on top left side representing the page', () => {
-    renderWithRouter('/');
+    renderWithRouter('/about');
     const pageEmote = screen.getByRole('img', { name: 'pageEmote' });
     expect(pageEmote).toBeDefined();
   });
