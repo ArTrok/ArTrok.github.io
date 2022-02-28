81f44241141aa1befbf7739278dfcba92b2dda27
Arthur <arthurteixeira0805@gmail.com>
2022-02-28 15:23:23 -0300

    🍱 asset: added images to files

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,20 +9,24 @@ export default function MainPage() {
   return (
     <div>
       <NavBar />
-      <div className='text_content_container'>
-        <h1 id="title">Arthur Teixeira Santos</h1>
-        <p id='description' data-testid="brief_description">
-          Olá, meu nome é Arthur e estou buscando ser reconhecido como desenvolvedor
-          através de minhas ações. Na jornada para a realização de um sonho.
-        </p>
-        <a href='https://github.com/ArTrok' aria-label='GitHub_L'>
-          <img src={ GitHubIcon } alt="GitHub_Icon" />
-        </a>
-        <a href='https://www.linkedin.com/in/arthur-teixeira-santos/' aria-label='LinkedIn_L'>
-          <img src={ LinkedInIcon } alt="LinkedIn_Icon" />
-        </a>
+      <div className='main_content_container flex'>
+        <div className='text_content_container'>
+          <h1 id="title">Arthur Teixeira Santos</h1>
+          <p id='description' data-testid="brief_description">
+            Olá, meu nome é Arthur e estou buscando ser reconhecido como desenvolvedor
+            através de minhas ações. Na jornada para a realização de um sonho.
+          </p>
+          <a href='https://github.com/ArTrok' aria-label='GitHub_L'>
+            Conheça o meu GitHub
+            <img src={ GitHubIcon } alt="GitHub_Icon" className="w-24" />
+          </a>
+          <a href='https://www.linkedin.com/in/arthur-teixeira-santos/' aria-label='LinkedIn_L'>
+            Conheça o meu LinkedIn
+            <img src={ LinkedInIcon } alt="LinkedIn_Icon" className="w-24" />
+          </a>
+        </div>
+        <img src={ ProfilePicture } alt="Arthur T. Santos's face" width="300px" id='profile-pic' data-testid="profile_pic" />
       </div>
-      <img src={ ProfilePicture } alt="Arthur T. Santos's face" width="300px" id='profile-pic' data-testid="profile_pic" />
     </div>
   )
 }
