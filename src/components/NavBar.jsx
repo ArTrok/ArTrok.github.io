afd30a370ef0fe5665aac3e3fb8d04d1fa76ef78
Arthur <arthurteixeira0805@gmail.com>
2022-03-05 19:30:06 -0300

    🚧 💄 style: adding better responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import AboutPageIcon from '../images/meLirik.png';
 import ProjectsPageIcon from '../images/Hackermans.gif'; 
 import SkillsPageIcon from '../images/tiLirik.png'; 
 import ContactPageIcon from '../images/lirikHullo.png';
+import MenuIcon from '../images/MenuIcon.svg';
 import './NavBar.css';
 import { motion } from 'framer-motion';
 
@@ -60,34 +61,45 @@ const NavBar = () => {
     }
   };
 
+  function toggleMenu () {
+    const navLinks = document.getElementById('navLinks');
+    const menuEmote = document.getElementById('menuEmote');
+    navLinks.classList.toggle('show');
+    menuEmote.classList.toggle('show');
+  }
+
   return (
-    <motion.nav variants={skillVariantMain} animate="visible" initial="hidden" className="text-cream bg-light-blue bg-opacity-75" >
-      <motion.img className='nav-button0' whileHover={{ scale: 1.2 }} transition={{ type: 'spring', stiffness: 500}} whileTap={{ scale: 0.8, rotate: -3 }} alt="Emote regarding page" src={ pageIcon } aria-label="pageEmote" />
-      <Link variants={skillVariant} animate="visible" initial="hidden" to="/" className='nav-button1' data-testid="Home_b">
-      <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
-        Home
-      </motion.div>
-      </Link>
-      <Link variants={skillVariant} animate="visible" initial="hidden" to="/about" className='nav-button2' data-testid="About_b">
-      <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
-        Sobre
-      </motion.div>
-      </Link>
-      <Link variants={skillVariant} animate="visible" initial="hidden" to="/projects" className='nav-button3' data-testid="Projects_b">
-      <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
-        Projetos
-      </motion.div>
-      </Link>
-      <Link variants={skillVariant} animate="visible" initial="hidden" to="/skills" className='nav-button4' data-testid="Skills_b">
-      <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
-        Habilidades
-      </motion.div>
-      </Link>
-      <Link variants={skillVariant} animate="visible" initial="hidden" to="/contact" className='nav-button5' data-testid="Contact_b">
-      <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
-        Contato
-      </motion.div>
-      </Link>
+    <motion.nav variants={skillVariantMain} animate="visible" initial="hidden" className="navBar text-cream bg-light-blue bg-opacity-75" >
+      <button onClick={() => toggleMenu()} className="menuIcon" id="menuIcon"><img src={MenuIcon} alt="Menu Icon" /></button>
+      <motion.img className='nav-button0' id='menuEmote' whileHover={{ scale: 1.2 }} transition={{ type: 'spring', stiffness: 500}} whileTap={{ scale: 0.8, rotate: -3 }} alt="Emote regarding page" src={ pageIcon } aria-label="pageEmote" />
+      <div className='navLinks' id='navLinks'>
+        <Link variants={skillVariant} animate="visible" initial="hidden" to="/" className='nav-button1' data-testid="Home_b">
+        <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
+          Home
+        </motion.div>
+        </Link>
+        <Link variants={skillVariant} animate="visible" initial="hidden" to="/about" className='nav-button2' data-testid="About_b">
+        <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
+          Sobre
+        </motion.div>
+        </Link>
+        <Link variants={skillVariant} animate="visible" initial="hidden" to="/projects" className='nav-button3' data-testid="Projects_b">
+        <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
+          Projetos
+        </motion.div>
+        </Link>
+        <Link variants={skillVariant} animate="visible" initial="hidden" to="/skills" className='nav-button4' data-testid="Skills_b">
+        <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
+          Habilidades
+        </motion.div>
+        </Link>
+        <Link variants={skillVariant} animate="visible" initial="hidden" to="/contact" className='nav-button5' data-testid="Contact_b">
+        <motion.div className=' rounded-md bg-dark-blue shadow-2xl' whileHover={{ scale: 1.1, rotate: 3 }} whileTap={{ scale: 0.8, rotate: -3 }} transition={{ stiffness: 500, type: 'spring'}}>
+          Contato
+        </motion.div>
+        </Link>
+
+      </div>
   </motion.nav>
   )
 }
