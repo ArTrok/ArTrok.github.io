08656696fc3d274f1ff10c9ec7fa7a618402b93a
Arthur <arthurteixeira0805@gmail.com>
2022-03-05 23:45:04 -0300

    💄 style: fixing positioning

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -20,16 +20,18 @@ export default function MainPage() {
               Olá, meu nome é Arthur e estou buscando ser reconhecido como desenvolvedor
               através de minhas ações. Na jornada para a realização de um sonho.
             </motion.p>
-            <motion.a id='iconLink1' whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='linkContainer justify-around shadow-2xl flex items-center rounded-md my-3 text-dark-blue bg-light-blue p-3'>
-              <p>Conheça o meu GitHub</p>
-              <img src={ GitHubIcon } alt="GitHub_Icon" className="iconLink" />
-            </motion.a>
-            <motion.a id='iconLink2' whileHover={{ scale: 1.1, rotate: -1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: 1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/' target="_blank" rel='noreferrer' aria-label='LinkedIn_L' className='linkContainer shadow-2xl justify-around flex items-center rounded-md my-3 text-dark-blue bg-light-blue p-3'>
-            <p>Conheça o meu LinkedIn</p>
-              <img src={ LinkedInIcon } alt="LinkedIn_Icon" className="iconLink" />
-            </motion.a>
             <motion.img whileHover={{ scale: 1.1, rotate: 5 }} transition={{ type: "just", stiffness: 500}} whileTap={{ scale: 0.8, rotate: -5
             }} src={ ProfilePicture } alt="Arthur T. Santos's face" id='profile-pic' className="rounded-md mr-5" data-testid="profile_pic" />
+            <div className='mediaLinks'>
+              <motion.a id='iconLink1' whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='linkContainer justify-around shadow-2xl flex items-center rounded-md my-3 text-dark-blue bg-light-blue p-3'>
+                <p>Conheça o meu GitHub</p>
+                <img src={ GitHubIcon } alt="GitHub_Icon" className="iconLink" />
+              </motion.a>
+              <motion.a id='iconLink2' whileHover={{ scale: 1.1, rotate: -1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: 1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/' target="_blank" rel='noreferrer' aria-label='LinkedIn_L' className='linkContainer shadow-2xl justify-around flex items-center rounded-md my-3 text-dark-blue bg-light-blue p-3'>
+              <p>Conheça o meu LinkedIn</p>
+                <img src={ LinkedInIcon } alt="LinkedIn_Icon" className="iconLink" />
+              </motion.a>
+            </div>  
         </motion.div>
           </div>
         <MadeByMeSeal />
