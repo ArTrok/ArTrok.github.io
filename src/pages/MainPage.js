d77f7bb48a5a193d9a4efaebb779ec8d07408798
Arthur <arthurteixeira0805@gmail.com>
2022-03-05 23:58:17 -0300

    💄 style: maybe it is enough

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -16,12 +16,14 @@ export default function MainPage() {
         <motion.div animate={{opacity: 1}} initial={{opacity: 0}} transition={{ duration: 1 }} className='main_content_container grid'>
 
             <motion.h1 whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} id="title" className="text-dark-blue shadow-2xl bg-light-blue rounded-md my-3 text-center p-4">Arthur Teixeira Santos</motion.h1>
+            <div className='desc_pic'>
             <motion.p id='description' data-testid="brief_description" whileHover={{ scale: 1.1, rotate: -1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: 1}} className="description text-dark-blue shadow-2xl bg-light-blue rounded-md my-3 p-3">
               Olá, meu nome é Arthur e estou buscando ser reconhecido como desenvolvedor
               através de minhas ações. Na jornada para a realização de um sonho.
             </motion.p>
             <motion.img whileHover={{ scale: 1.1, rotate: 5 }} transition={{ type: "just", stiffness: 500}} whileTap={{ scale: 0.8, rotate: -5
             }} src={ ProfilePicture } alt="Arthur T. Santos's face" id='profile-pic' className="rounded-md mr-5" data-testid="profile_pic" />
+            </div>
             <div className='mediaLinks'>
               <motion.a id='iconLink1' whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='linkContainer justify-around shadow-2xl flex items-center rounded-md my-3 text-dark-blue bg-light-blue p-3'>
                 <p>Conheça o meu GitHub</p>
