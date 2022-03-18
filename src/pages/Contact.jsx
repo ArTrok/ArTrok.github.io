14d9dd9430bfe2b8209feccbbc6f553499b2c79a
Arthur <arthurteixeira0805@gmail.com>
2022-03-17 22:05:00 -0300

    🚧 wip: triyng to improve contact page with alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
   const [messageSent, setMessageSent] = useState(false);
   const skillVariantMain = {
     hidden: {
@@ -24,31 +25,46 @@ const Contact = () => {
     }
   };
 
+  function isDisabledValidate () {
+    const emailChecking = /^([a-z\d._-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i
+      .test(email);
+    const nameChecking = name.length > 3;
+    const messageChecking = message.length > 100;
+
+    if (emailChecking && nameChecking && messageChecking) return setIsDisabled(false);
+    setIsDisabled(true);
+  }
+
   function handleChangeEmail ({target}) {
     setEmail(target.value);
+    isDisabledValidate();
   }
 
   function handleChangeName ({target}) {
     setName(target.value);
+    isDisabledValidate();
   }
 
   function handleChangeMessage ({target}) {
     setMessage(target.value);
+    isDisabledValidate();
   }
 
   function sendEmail () {
-    Axios.post(process.env.REACT_APP_ENDPOINT_EMAIL, {email, message, name})
-      .then(res => {
-        if(res.data.success) {
-          setMessageSent(true);
-        } else {
-          setMessageSent(false);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        setMessageSent(false);
-      });
+    // Axios.post(process.env.REACT_APP_ENDPOINT_EMAIL, {email, message, name})
+    //   .then(res => {
+    //     if(res.data.success) {
+    //       setMessageSent(true);
+    //     } else {
+    //       setMessageSent(false);
+    //     }
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //     setMessageSent(false);
+    //   });
+    setMessageSent(true);
+
   };
 
   return (
@@ -61,19 +77,19 @@ const Contact = () => {
       <motion.input onChange={handleChangeName} animate={{opacity: 1}} initial={{opacity: 0}} style={{opacity: 1, width: "216px"}} type="text" aria-label='Name' placeholder='Nome Completo' id='name' className='bg-light-blue text-cream h-10 rounded-md p-2 name'/>
       <motion.input onChange={handleChangeEmail} animate={{opacity: 1}} initial={{opacity: 0}} type="email" aria-label="Email" placeholder='E-Mail' id="email" className='bg-light-blue text-cream rounded-md p-2'/>
         <motion.textarea onChange={handleChangeMessage} animate={{opacity: 1}} initial={{opacity: 0}} aria-label="Message" id="message" rows="4" cols="45" placeholder='Mensagem' className='message resize-none bg-light-blue text-cream rounded-md p-2 self-stretch' />
-      <motion.button onClick={sendEmail} animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: -3}} transition={{ type: "spring", stiffness: 2000}} whileTap={{ scale: 0.9, rotate: 3}} aria-label='Send_It' className='text-beige-green bg-light-blue w-40 rounded-md font-bold send'>Enviar</motion.button>
+      <motion.button onClick={sendEmail} disabled={isDisabled} animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: -3}} transition={{ type: "spring", stiffness: 2000}} whileTap={{ scale: 0.9, rotate: 3}} aria-label='Send_It' className='text-beige-green bg-light-blue w-40 rounded-md font-bold send'>Enviar</motion.button>
+      <div className='text-cream'>{messageSent && 'Mensagem Enviada'}</div>
       <div className="flex gap-5">
-        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='flex items-center border-2 border-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
+        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
         GitHub
           <img src={ GitHubIcon } alt="GitHub_Icon" className="ml-3" />
         </motion.a>
-        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/' target="_blank" rel='noreferrer' aria-label='LinkedIn_L' className='flex items-center border-2 border-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
+        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/' target="_blank" rel='noreferrer' aria-label='LinkedIn_L' className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
         LinkedIn
           <img src={ LinkedInIcon } alt="LinkedIn_Icon" className="ml-3" />
         </motion.a>
       </div>
     </motion.div>
-    <div>{messageSent}</div>
     <MadeByMeSeal />
     </div>
   </div>
