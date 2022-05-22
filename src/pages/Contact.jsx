a6eba2cc36e47f2b1a2299b9e452d6a07a623b0d
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 <artrok@github.com>
2022-05-21 22:05:25 -0300

    🚨 lint: small improve in readability

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -74,26 +74,103 @@ const Contact = () => {
     <div className="bg-deep-purple bg-opacity-50">
 
     <NavBar />
-    <motion.div variants={skillVariantMain} animate="visible" initial="hidden" className='main_content_container5 gap-5 p-3 justify-items-center bg-deep-purple rounded-md'>
-      <motion.h2 animate={{opacity: 1}} initial={{opacity: 0}} data-testid="ContactMe" className='text-cream font-bold text-4xl'>{language === 'portuguese' ? 'Entre em Contato' : 'Contact Me'}</motion.h2>
-      <motion.input onChange={handleChangeName} animate={{opacity: 1}} initial={{opacity: 0}} style={{opacity: 1, width: "216px"}} type="text" aria-label='Name' placeholder={language === 'portuguese' ? 'Nome Completo' : 'Full Name'} id='name' className='bg-light-blue text-cream h-10 rounded-md p-2 name'/>
-      <motion.input onChange={handleChangeEmail} animate={{opacity: 1}} initial={{opacity: 0}} type="email" aria-label="Email" placeholder='E-Mail' id="email" className='bg-light-blue text-cream rounded-md p-2'/>
+    <motion.div
+      variants={skillVariantMain}
+      animate="visible"
+      initial="hidden"
+      className='main_content_container5 gap-5 p-3 justify-items-center bg-deep-purple rounded-md'>
+
+      <motion.h2
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        data-testid="ContactMe"
+        className='text-cream font-bold text-4xl'>{language === 'portuguese' ? 'Entre em Contato' : 'Contact Me'}
+      </motion.h2>
+
+      <motion.input
+        onChange={handleChangeName}
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        style={{opacity: 1, width: "216px"}}
+        type="text" aria-label='Name'
+        placeholder={language === 'portuguese' ? 'Nome Completo' : 'Full Name'}
+        id='name'
+        className='bg-light-blue text-cream h-10 rounded-md p-2 name'/>
+
+      <motion.input
+        onChange={handleChangeEmail}
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        type="email"
+        aria-label="Email"
+        placeholder='E-Mail'
+        id="email"
+        className='bg-light-blue text-cream rounded-md p-2'/>
+
         <div className="message rounded-md p-2 flex flex-col">
-        <motion.textarea onChange={handleChangeMessage} animate={{opacity: 1}} initial={{opacity: 0}} aria-label="Message" id="message" rows="4" cols="45" placeholder={language === 'portuguese' ? 'Mensagem' : 'Message'} className='resize-none bg-light-blue text-cream rounded self-stretch p-2' />
+        <motion.textarea
+          onChange={handleChangeMessage}
+          animate={{opacity: 1}}
+          initial={{opacity: 0}}
+          aria-label="Message"
+          id="message"
+          rows="4"
+          cols="45"
+          placeholder={language === 'portuguese' ? 'Mensagem' : 'Message'}
+          className='resize-none bg-light-blue text-cream rounded self-stretch p-2' />
         <div className="text-cream self-end">{message.length} {language === 'portuguese' ? 'caracteres' : 'characters'}</div>
         </div> 
-      <motion.button onClick={sendEmail} animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: -3}} transition={{ type: "spring", stiffness: 2000}} whileTap={{ scale: 0.9, rotate: 3}} aria-label='Send_It' className='text-beige-green bg-light-blue w-40 rounded-md font-bold send'>{language === 'portuguese' ? 'Enviar' : 'Submit'}</motion.button>
+
+      <motion.button
+        onClick={sendEmail}
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        whileHover={{ scale: 1.1, rotate: -3}}
+        transition={{ type: "spring", stiffness: 2000}}
+        whileTap={{ scale: 0.9, rotate: 3}}
+        aria-label='Send_It'
+        className='text-beige-green bg-light-blue w-40 rounded-md font-bold send'>{language === 'portuguese' ? 'Enviar' : 'Submit'}
+      </motion.button>
+
       <div className='text-cream'>{messageSent}</div>
-      <motion.p className='text-cream emailText' onClick={ copyEmail } whileHover={{ scale: 1.1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9 }}>{language === 'portuguese' ? 'copiar email: arthurteixeira0805@gmail.com' : 'copy email: arthurteixeira0805@gmail.com'}</motion.p>
+      
+      <motion.p
+        className='text-cream emailText'
+        onClick={ copyEmail }
+        whileHover={{ scale: 1.1}}
+        transition={{ type: "spring", stiffness: 500}}
+        whileTap={{ scale: 0.9 }}>{language === 'portuguese' ? 'copiar email: arthurteixeira0805@gmail.com' : 'copy email: arthurteixeira0805@gmail.com'}
+      </motion.p>
+
       <div className="flex gap-5">
-        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok' target="_blank" rel='noreferrer' aria-label='GitHub_L' className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
+        <motion.a
+          animate={{opacity: 1}}
+          initial={{opacity: 0}}
+          whileHover={{ scale: 1.1, rotate: 1}}
+          transition={{ type: "spring", stiffness: 500}}
+          whileTap={{ scale: 0.9, rotate: -1}} href='https://github.com/ArTrok'
+          target="_blank"
+          rel='noreferrer'
+          aria-label='GitHub_L'
+          className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
         GitHub
           <img src={ GitHubIcon } alt="GitHub_Icon" className="ml-3" />
         </motion.a>
-        <motion.a animate={{opacity: 1}} initial={{opacity: 0}} whileHover={{ scale: 1.1, rotate: 1}} transition={{ type: "spring", stiffness: 500}} whileTap={{ scale: 0.9, rotate: -1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/' target="_blank" rel='noreferrer' aria-label='LinkedIn_L' className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
+
+        <motion.a
+          animate={{opacity: 1}}
+          initial={{opacity: 0}}
+          whileHover={{ scale: 1.1, rotate: 1}}
+          transition={{ type: "spring", stiffness: 500}}
+          whileTap={{ scale: 0.9, rotate: -1}} href='https://www.linkedin.com/in/arthur-teixeira-santos/'
+          target="_blank"
+          rel='noreferrer'
+          aria-label='LinkedIn_L'
+          className='flex items-center border-2 border-cream text-cream bg-light-blue shadow-xl p-1 rounded-md link_button'>
         LinkedIn
           <img src={ LinkedInIcon } alt="LinkedIn_Icon" className="ml-3" />
         </motion.a>
+
       </div>
     </motion.div>
     <MadeByMeSeal />
